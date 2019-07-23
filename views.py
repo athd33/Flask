8e9223f1813bd66d03e69056a519ee6d0f140e52
athd33 <antoinecours@gmx.fr>
2019-07-23 13:31:12 +0200

    jquery added , click added

--- views.py
+++ views.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('pages/home.html')
+    return render_template('layouts/default.html')
 
 @app.route('/search')
 def search():
     return render_template('pages/search.html')
+
 
 
 if __name__ == "__main__":
