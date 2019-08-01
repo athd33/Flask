069e9c77ed94d0ed2aa161181af29f841483f1d2
athd33 <antoinecours@gmx.fr>
2019-08-01 08:29:56 +0200

    update project

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, url_for, redirect
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("/home.html", message="Home page")
+
+@app.route("/search")
+def search():
+     return render_template("/search.html")
+
+@app.route("/about")
+def about():
+     return render_template("/about.html", message="About page")
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+     error = ""
+     if request.method == "POST":
+          attempted_username = request.form['username']
+          attempted_password = request.form['password']
+
+          if attempted_username == "admin" and attempted_password == "password":
+               return redirect(url_for('search'))
+          else:
+               error = "Invalid credentials, try again..."
+     return render_template("/login.html", error=error)
+
+
+
+if __name__ == "__main__":
+     app.run( debug=True)
