814cb65a88aa5f19a1e7acea9547cb2bc99f7fd0
athd33 <antoinecours@gmx.fr>
2019-08-03 21:36:46 +0200

    first form infos

--- app.py
+++ app.py
@@ -1,42 +1,53 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 from dbconnect import connection
+from passlib.hash import sha256_crypt
+from pymysql import escape_string as thwart
 
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("/home.html", message="Home page")
 
+
 @app.route("/search")
 def search():
-     return render_template("/search.html")
+    return render_template("/search.html")
+
 
 @app.route("/about")
 def about():
-     return render_template("/about.html", message="About page")
+    return render_template("/about.html", message="About page")
 
-@app.route("/login", methods=['GET','POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-     error = ""
-     if request.method == "POST":
-          attempted_username = request.form['username']
-          attempted_password = request.form['password']
+    error = ""
+    if request.method == "POST":
+        attempted_username = request.form['username']
+        attempted_password = request.form['password']
 
-          if attempted_username == "admin" and attempted_password == "password":
-               return redirect(url_for('search'))
-          else:
-               error = "Invalid credentials, try again..."
-     return render_template("/login.html", error=error)
+        if attempted_username == "admin" and attempted_password == "password":
+            return redirect(url_for('search'))
+        else:
+            error = "Invalid credentials, try again..."
+
+    return render_template("/login.html", error=error)
 
 
-@app.route("/register", methods=['GET','POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register_page():
-     try:
-          c, conn = connection()
-          return "ok"
-     except Exception as e:
-          return(str(e))
+    error = ""
+    if request.method == "POST":
+        username = request.form['username']
+        if username != "":
+            return render_template('search.html', username=username)
+
+    return render_template('/register.html', error=error)
+
+
+
 
 
 
@@ -44,4 +55,5 @@ def register_page():
 
 
 if __name__ == "__main__":
-     app.run( debug=True)
+ 
+    app.run(debug=True)
