704129e45e365f06913f65c2fcf64f26b07bcc70
athd33 <antoinecours@gmx.fr>
2019-08-07 12:55:23 +0200

    update db

--- api.py
+++ api.py
@@ -1,7 +1,17 @@
 import requests
 import pprint
 
-r = requests.get("https://fr.openfoodfacts.org/categories&json=1")
+
+'''
+r = requests.get("https://fr.openfoodfacts.org/additives&json=1")
+'''
+
+payload = {"search_terms": "Lindt, "json": 1}
+r = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?search_terms=Lindt&json=1")
+
+print(r)
+
+'''
 
 data = r.json()
 
@@ -10,3 +20,5 @@ c = data['tags']
 
 for i in c:
     print(i['name'])
+
+'''
